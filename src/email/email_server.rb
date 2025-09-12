7deb3535395d89ce6247b5d9dcf1e9d06403e5d2
Paweł J. Wal <pawel@betterstack.com>
2025-09-12 13:51:59 +0200

    deinstrument email service

--- src/email/email_server.rb
+++ src/email/email_server.rb
@@ -8,9 +8,6 @@ require "open_feature/sdk"
 require "openfeature/flagd/provider"
 # require "open_feature/flagd_provider"
 
-require "opentelemetry/sdk"
-require "opentelemetry/exporter/otlp"
-require "opentelemetry/instrumentation/sinatra"
 
 set :port, ENV["EMAIL_PORT"]
 
@@ -26,31 +23,20 @@ OpenFeature::SDK.configure do |config|
   config.set_provider(flagd_client)
 end
 
-OpenTelemetry::SDK.configure do |c|
-  c.use "OpenTelemetry::Instrumentation::Sinatra"
-end
 
 post "/send_order_confirmation" do
   data = JSON.parse(request.body.read, object_class: OpenStruct)
 
-  # get the current auto-instrumented span
-  current_span = OpenTelemetry::Trace.current_span
-  current_span.add_attributes({
-    "app.order.id" => data.order.order_id,
-  })
 
   send_email(data)
 
 end
 
 error do
-  OpenTelemetry::Trace.current_span.record_exception(env['sinatra.error'])
+  # Error handling
 end
 
 def send_email(data)
-  # create and start a manual span
-  tracer = OpenTelemetry.tracer_provider.tracer('email')
-  tracer.in_span("send_email") do |span|
     # Check if memory leak flag is enabled
     client = OpenFeature::SDK.build_client
     memory_leak_multiplier = client.fetch_number_value(flag_key: "emailMemoryLeak", default_value: 0)
@@ -73,11 +59,5 @@ def send_email(data)
       Mail::TestMailer.deliveries.clear
     end
 
-    span.set_attribute("app.email.recipient", data.email)
-    puts "Order confirmation email sent to: #{data.email}"
-  end
-  # manually created spans need to be ended
-  # in Ruby, the method `in_span` ends it automatically
-  # check out the OpenTelemetry Ruby docs at: 
-  # https://opentelemetry.io/docs/instrumentation/ruby/manual/#creating-new-spans 
+  puts "Order confirmation email sent to: #{data.email}"
 end
